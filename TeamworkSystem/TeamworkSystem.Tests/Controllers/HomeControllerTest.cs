bfbac91e0b9d7448fc233794a13b3dcec88a5ac6
DimitarLilov <dimitar.lilov.mici@gmail.com>
2017-05-04 11:59:06 +0300

    add projects photos

--- TeamworkSystem/TeamworkSystem.Tests/Controllers/HomeControllerTest.cs
+++ TeamworkSystem/TeamworkSystem.Tests/Controllers/HomeControllerTest.cs
@@ -20,30 +20,6 @@ namespace TeamworkSystem.Tests.Controllers
             Assert.IsNotNull(result);
         }
 
-        [TestMethod]
-        public void About()
-        {
-            // Arrange
-            HomeController controller = new HomeController();
-
-            // Act
-            ViewResult result = controller.About() as ViewResult;
-
-            // Assert
-            Assert.AreEqual("Your application description page.", result.ViewBag.Message);
-        }
-
-        [TestMethod]
-        public void Contact()
-        {
-            // Arrange
-            HomeController controller = new HomeController();
-
-            // Act
-            ViewResult result = controller.Contact() as ViewResult;
-
-            // Assert
-            Assert.IsNotNull(result);
-        }
+      
     }
 }
